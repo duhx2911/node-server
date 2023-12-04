3fdd4762175991003aaef50c308bf8ff6ee8416a
XuanDuc <90120804+duhx2911@users.noreply.github.com>
2023-12-04 15:51:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '13.238.195.139'; // Your server ip address
-const port = 3000;
+const port = 8000;
 
 const server = http.createServer((req, res) => {
 
